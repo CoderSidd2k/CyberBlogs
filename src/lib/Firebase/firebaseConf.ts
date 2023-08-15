2037fbe4c670ac20b7c4c6a8b248cb669e46935c
Gaurau Siddarth S <90812206+CoderSidd2k@users.noreply.github.com>
2023-08-15 23:37:20 +0530

    Update firebaseConf.ts

--- src/lib/Firebase/firebaseConf.ts
+++ src/lib/Firebase/firebaseConf.ts
@@ -6,15 +6,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBzTedZ3-720u6hfeqWFErA5VXUCZnjs2Y",
-  authDomain: "cyber-blogs-b1a52.firebaseapp.com",
-  projectId: "cyber-blogs-b1a52",
-  storageBucket: "cyber-blogs-b1a52.appspot.com",
-  messagingSenderId: "921860967396",
-  appId: "1:921860967396:web:6d93fb970baac66b9c55f1",
-  measurementId: "G-J6WHD1TDM3"
-};
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
